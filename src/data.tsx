730fdf557fadcf58936a4373297420b875561e87
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-09-18 22:25:38 +0530

    Dummy Data Added

---
+++ src/data.tsx
@@ -0,0 +1,92 @@
+export type ContactType = {
+  id: number;
+  name: string;
+  email: string;
+  group: number;
+};
+
+export const contacts: ContactType[] = [
+  {
+    id: 1,
+    name: "Walter White",
+    email: "walter.white@hotmail.com",
+    group: 1,
+  },
+  {
+    id: 2,
+    name: "Jesse Pinkman",
+    email: "jessee.pinkman@hotmail.com",
+    group: 1,
+  },
+  {
+    id: 3,
+    name: "Saul Goodman",
+    email: "saul.goodman@gmail.com",
+    group: 4,
+  },
+  {
+    id: 4,
+    name: "Gustavo Fring",
+    email: "gustavo.fring@hotmai.com",
+    group: 2,
+  },
+  {
+    id: 5,
+    name: "Gale Boetticher",
+    email: "gale.boetticher@hotmai.com",
+    group: 1,
+  },
+  {
+    id: 6,
+    name: "Huey",
+    email: "huey@gmail.com",
+    group: 3,
+  },
+  {
+    id: 6,
+    name: "Mike Ehrmantraut",
+    email: "mike.ehrmantraut@gmail.com",
+    group: 3,
+  },
+  {
+    id: 7,
+    name: "Tuco Salamanca",
+    email: "tuco.salamanca@hotmai.com",
+    group: 2,
+  },
+  {
+    id: 8,
+    name: "Hector Salamanca",
+    email: "hector.salamanca@hotmai.com",
+    group: 2,
+  },
+];
+
+export type GroupType = {
+  id: number;
+  name: string;
+  members: number;
+};
+
+export const groups: GroupType[] = [
+  {
+    id: 1,
+    name: "Cooks",
+    members: 3,
+  },
+  {
+    id: 2,
+    name: "Distributors",
+    members: 3,
+  },
+  {
+    id: 3,
+    name: "Security",
+    members: 2,
+  },
+  {
+    id: 4,
+    name: "Lawyers",
+    members: 1,
+  },
+];
